17fd5255c3b130e453860cf2949c6a328f7a27eb
Oliverngu <138084319+Oliverngu@users.noreply.github.com>
2025-08-13 10:55:31 +0200

    Update state-get.js

--- netlify/functions/state-get.js
+++ netlify/functions/state-get.js
@@ -1,12 +1,7 @@
 import { getStore } from "@netlify/blobs";
-
 export default async () => {
   const data = await getStore("mintshift").get("state.json");
-  return new Response(data ?? "{}", {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-      "access-control-allow-origin": "*",
-    },
-  });
+  return new Response(data ?? "{}", { status:200, headers:{
+    "content-type":"application/json", "access-control-allow-origin":"*"
+  }});
 };
